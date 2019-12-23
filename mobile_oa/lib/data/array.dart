b772f63f7dc944065af384c2cf2d221fedbca6aa
Jie_github <cjieouba@outlook.com>
2019-12-23 17:20:22 +0800

    Update array.dart

--- mobile_oa/lib/data/array.dart
+++ mobile_oa/lib/data/array.dart
@@ -1,10 +1,10 @@
 
 const DATA = [
-  {"image": "http://218.76.139.140:81/Scripts/kindeditor/attached/image/20190318/20190318135929_6872.jpg", "id": 9689108, "title": "图片新闻1"},
-  {"image": "http://218.76.139.140:81/Scripts/kindeditor/attached/image/20190318/20190318140146_4531.jpg", "id": 9689102, "title": "图片新闻2"},
-  {"image": "http://218.76.139.140:81/Scripts/kindeditor/attached/image/20190318/20190318135929_6872.jpg", "id": 9689097, "title": "图片新闻3"},
-  {"image": "http://218.76.139.140:81/Scripts/kindeditor/attached/image/20190318/20190318140146_4531.jpg", "id": 9689053, "title": "图片新闻4"},
-  {"image": "http://218.76.139.140:81/Scripts/kindeditor/attached/image/20190318/20190318135929_6872.jpg", "id": 9668185, "title": "图片新闻5"}
+  {"image": "http://example/Scripts/kindeditor/attached/image/20190318/20190318135929_6872.jpg", "id": 9689108, "title": "图片新闻1"},
+  {"image": "http://example/Scripts/kindeditor/attached/image/20190318/20190318140146_4531.jpg", "id": 9689102, "title": "图片新闻2"},
+  {"image": "http://example/Scripts/kindeditor/attached/image/20190318/20190318135929_6872.jpg", "id": 9689097, "title": "图片新闻3"},
+  {"image": "http://example/Scripts/kindeditor/attached/image/20190318/20190318140146_4531.jpg", "id": 9689053, "title": "图片新闻4"},
+  {"image": "http://example/Scripts/kindeditor/attached/image/20190318/20190318135929_6872.jpg", "id": 9668185, "title": "图片新闻5"}
 ];
 
 const DATA2 = {
